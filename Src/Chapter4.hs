e61c28606dadb99aa94d969b931444524ede7ff5
ryiwamoto <konnpekigan@gmail.com>
2015-01-29 12:38:35 +0900

    Chapter4-1

--- Src/Chapter4.hs
+++ Src/Chapter4.hs
@@ -11,19 +11,27 @@ module Src.Chapter4
 
     -- (1.1)
     tri_number :: Int -> Int
-    tri_number n = undefined {- Rewrite HERE! -}
+    tri_number 0 = 0
+    tri_number n = n + tri_number (n - 1)
 
     -- (1.2)
     tetration :: Integer -> Integer -> Integer
-    tetration x n = undefined {- Rewrite HERE! -}
+    tetration x 0 = 1
+    tetration n m = n ^ tetration n (m - 1)
 
     -- (1.3)
     index :: Int -> [a] -> a
-    index n (x:_) = undefined {- Rewrite HERE! -}
+    index n [] = error "empty"
+    index n xs | n >= 0 && n > length xs = error "out of range"
+    index n xs = xs !! n
 
-    -- (1.4)
+    -- (1.4) 
     even_odd :: [Int] -> ([Int], [Int])
-    even_odd [] = undefined {- Rewrite HERE! -}
+    even_odd [] = ([], [])
+    even_odd (x:xs) 
+        | x `mod` 2 == 0 = (odds, x:evens) --cons
+        | otherwise = (x:odds, evens)
+        where (odds, evens) = even_odd xs --whereでパターンマッチができる
 
     -- (2.1)
     insert :: Ord a => [a] -> a -> [a]
